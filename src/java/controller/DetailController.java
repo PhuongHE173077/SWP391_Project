836982037fa24859ab2d3921de8fb16e4e2d20ae
BaoNQ <baonqhe170954@fpt.edu.vn>
2024-06-13 22:10:20 +0700

    confirmCV

--- src/java/controller/DetailController.java
+++ src/java/controller/DetailController.java
@@ -77,3 +77,92 @@ public class DetailController extends HttpServlet {
     }// </editor-fold>
 
 }
+
+//package controller;
+//
+//import dao.CvDao;
+//import dao.UserDao;
+//import entity.CvMentor;
+//import entity.User;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import service.SendEmail;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.List;
+//
+///**
+// *
+// * @author Acer
+// */
+//public class DetailController extends HttpServlet {
+//
+//    /**
+//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+//     * methods.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet DetailController</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet DetailController at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        int mentorId = Integer.parseInt(request.getParameter("id")); // Assuming the mentor ID is passed as a parameter
+//        
+//        CvDao cvDao = new CvDao(); // Initialize your CvDao
+//        CvMentor cvMentor = cvDao.getCvMentorByID(mentorId); // Fetch the CvMentor object
+//        
+//        request.setAttribute("cv", cvMentor);
+//        request.setAttribute("mid", mentorId);
+//        
+//        // Fetch the list of users and send emails
+//        UserDao userDao = new UserDao();
+//        List<User> users = userDao.getUser();
+//
+//        SendEmail sendEmailService = new SendEmail();
+//        String subject = "Mentor Details";
+//        String message = "Details of mentor with ID: " + mentorId;
+//
+//        for (User user : users) {
+//            String toMail = user.getEmail();
+//            // Sending the email
+//            sendEmailService.sendEmail(toMail, subject, message);
+//        }
+//
+//        request.getRequestDispatcher("detail.jsp").forward(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+//
+//}
